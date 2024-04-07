ecf0d7e920835dc79387a2a7a15f495a8a741de1
Solano Coronado Gabriel <Solanocoronado.g@gmail.com>
2024-04-06 19:38:11 -0600

    Proyeto 1 version final

---
+++ proyectos/Movie-Manager-App/src/main/java/tec/poo/proyectos/Movie.java
@@ -0,0 +1,77 @@
+package tec.poo.proyectos;
+
+
+import java.util.ArrayList;
+
+
+public class Movie {
+
+
+    //Declarate isntance
+    private String title;
+    private double  rating;
+    private String category;
+    private String review;
+    private String source;
+
+    /***
+     * Constructor method
+     *
+     *
+     */
+
+    public Movie(String title, String category, double rating, String review,String source){
+        this.title = title;
+        this.rating=rating;
+        this.review=review;
+        this.category=category;
+        this.source=source;
+
+    }
+    /***
+     * Getter and setter all of instances
+     */
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
---
+++ proyectos/Movie-Manager-App/src/readme.md
@@ -0,0 +1,13 @@
+1-Este es el formato de rutas en comando "C:\Users\Gabri\Documents\GASC\movie.dat"
+
+2-Formato rutas modo interactivo si se corre en idle C:\Users\Gabri\Documents\GASC\movie.dat
+
+3-Si en el modo piclo añases una ruta sin comillas la tomará como un nombre 
+y creara el archivo en el directorio del proyecto
+
+4-Hay problemas con los buffer, hay varios scanner.nextLine() para limpiarlos
+
+5-EL JAR SE LLAMA Movie_Manager_App 
+Ejemplo de como se ejecutan comandos
+$ java -jar target/Movie_Manager_App-1.0.0-jar-with-dependencies.jar list-movies-filter-by-category horror
+
